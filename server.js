ac9e4d2b9056cf9565292cc8778f7beba928c5f9
hydedall <hyde.dallas1@gmail.com>
2022-09-07 00:55:44 -0400

    seed

--- server.js
+++ server.js
@@ -26,7 +26,47 @@ db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+//ROUTES
+
+//SEED
+app.get('/products/seed', (req, res) => {
+    Product.deleteMany({}, (error, allProducts) => { });
+    Product.create([
+      {
+        name: 'Beans',
+        description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+        img: 'https://imgur.com/LEHS8h3.png',
+        price: 5,
+        qty: 99
+      }, {
+        name: 'Bones',
+        description: 'It\'s just a bag of bones.',
+        img: 'https://imgur.com/dalOqwk.png',
+        price: 25,
+        qty: 0
+      }, {
+        name: 'Bins',
+        description: 'A stack of colorful bins for your beans and bones.',
+        img: 'https://imgur.com/ptWDPO1.png',
+        price: 7000,
+        qty: 1
+      }
+    ],
+      (error, data) => {
+        res.redirect('/products')
+      }
+    );
+  });
+
+
 //INDEX
+app.get("/products" , function (req, res){
+    Product.find({}, (error, allProducts)=> {
+        res.render("index.ejs" , {
+            products: allProducts,
+        });
+    });
+});
 //NEW
 //DELETE
 //UPDATE
