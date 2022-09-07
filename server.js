d378c739dd6fffc193adfd7f35443e69b927a810
hydedall <hyde.dallas1@gmail.com>
2022-09-07 00:59:47 -0400

    update

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 //DATABASE CONNECTION -------------
 /////////////////////
 mongoose.connect(process.env.DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 //////////////////////
@@ -32,44 +32,57 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 app.get('/products/seed', (req, res) => {
     Product.deleteMany({}, (error, allProducts) => { });
     Product.create([
-      {
-        name: 'Beans',
-        description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-        img: 'https://imgur.com/LEHS8h3.png',
-        price: 5,
-        qty: 99
-      }, {
-        name: 'Bones',
-        description: 'It\'s just a bag of bones.',
-        img: 'https://imgur.com/dalOqwk.png',
-        price: 25,
-        qty: 0
-      }, {
-        name: 'Bins',
-        description: 'A stack of colorful bins for your beans and bones.',
-        img: 'https://imgur.com/ptWDPO1.png',
-        price: 7000,
-        qty: 1
-      }
+        {
+            name: 'Beans',
+            description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+            img: 'https://imgur.com/LEHS8h3.png',
+            price: 5,
+            qty: 99
+        }, {
+            name: 'Bones',
+            description: 'It\'s just a bag of bones.',
+            img: 'https://imgur.com/dalOqwk.png',
+            price: 25,
+            qty: 0
+        }, {
+            name: 'Bins',
+            description: 'A stack of colorful bins for your beans and bones.',
+            img: 'https://imgur.com/ptWDPO1.png',
+            price: 7000,
+            qty: 1
+        }
     ],
-      (error, data) => {
-        res.redirect('/products')
-      }
+        (error, data) => {
+            res.redirect('/products')
+        }
     );
-  });
+});
 
 
 //INDEX
-app.get("/products" , function (req, res){
-    Product.find({}, (error, allProducts)=> {
-        res.render("index.ejs" , {
+app.get("/products", function (req, res) {
+    Product.find({}, (error, allProducts) => {
+        res.render("index.ejs", {
             products: allProducts,
         });
     });
 });
+
 //NEW
+app.get("/products/new" , function (req, res){
+    res.render("new.ejs")
+});
+
 //DELETE
+
 //UPDATE
+app.put('/products/:id', (req, res) => {
+    Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, // LETS IT RECIEVE THE UPDATED DOCUMENT AND OVERRIDES PREVIOUS DATA WITH req.body
+    }, () => {
+        res.redirect(`/products/${req.params.id}`)
+    })
+})
 //CREATE
 //EDIT
 //SHOW
@@ -78,5 +91,5 @@ app.get("/products" , function (req, res){
 // LISTENER
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log("listening to port...", PORT);
+    console.log("listening to port...", PORT);
 })
