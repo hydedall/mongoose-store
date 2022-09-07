09596e675174559d573c3bd5f96417f3c5cae75c
hydedall <hyde.dallas1@gmail.com>
2022-09-07 00:53:31 -0400

    middleware database connection/errors

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
+require('dotenv').config();
 //////////////
 //DEPENDENCIES ------------------
 //////////////
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const products = require('./models/products.js');
 const methodOverride = require("method-override");
-require('dotenv').config();
+
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
 
 /////////////////////
 //DATABASE CONNECTION -------------
 /////////////////////
-
-
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 //////////////////////
 //DATABASE CONNECTION ERROR / SUCCESS
 //////////////////////
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 //INDEX
 //NEW
@@ -30,5 +38,5 @@ require('dotenv').config();
 // LISTENER
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log("listening in port...", PORT);
+  console.log("listening to port...", PORT);
 })
